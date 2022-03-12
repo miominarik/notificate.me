04e68eae242b7625ff00aebd6ea74992def1419f
Miroslav Minárik <miominarik2@gmail.com>
2022-03-12 18:29:34 +0100

    Settings Translation

---
+++ lang/en/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'settings_header' => 'Settings',
+    'enable_email_notif' => 'Enable email notifications',
+    'time_when_notif' => 'Notification time',
+    'time_when_notif_help' => 'The time when the notification should be sent that day',
+    'yes' => 'Yes',
+    'no' => 'No',
+    'send_btn' => 'Save'
+];
