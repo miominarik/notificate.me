791c0a184d86a2c0e04aeed796a715fec3e1fed4
Miroslav Minárik <miominarik2@gmail.com>
2022-05-07 21:10:51 +0200

    Pridané gdpr do appky

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -69,15 +69,19 @@
 
                             <hr class="my-4">
                             <div class="d-grid gap-2 mb-2">
-                                <a href="{{route('oauth.github-login')}}" class="btn btn-block btn-social btn-github">
+                                <a href="{{ route('oauth.github-login') }}" class="btn btn-block btn-social btn-github">
                                     <span class="fa-brands fa-github"></span> {{ __('auth.register_github') }}
                                 </a>
                             </div>
                             <div class="d-grid gap-2 mb-2">
-                                <a href="{{route('oauth.google-login')}}" class="btn btn-block btn-social btn-google">
+                                <a href="{{ route('oauth.google-login') }}" class="btn btn-block btn-social btn-google">
                                     <span class="fa-brands fa-google"></span> {{ __('auth.register_google') }}
                                 </a>
                             </div>
+                            <small>
+                                Registráciou súhlasíte so
+                                <a href="{{ route('gdpr') }}" target="_blank">spracovaním osobných údajov</a>.
+                            </small><br>
                             <small>
                                 This site is protected by reCAPTCHA and the Google
                                 <a href="https://policies.google.com/privacy">Privacy Policy</a> and
