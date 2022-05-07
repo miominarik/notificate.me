70c7a5b449304c2ae0de259c53fa156a9aa6c1af
Miroslav Minárik <miominarik2@gmail.com>
2022-05-07 21:06:35 +0200

    Pridané menu na homePage

--- resources/views/home/includes/header.blade.php
+++ resources/views/home/includes/header.blade.php
@@ -6,13 +6,24 @@
         </div>
 
         <nav id="navbar" class="navbar">
-            <ul>
-                <li><a class="nav-link scrollto" href="#hero">{{ __('home_page.menu_home') }}</a></li>
-                <li><a class="nav-link scrollto" href="#services">{{ __('home_page.menu_services') }}</a></li>
-                <li><a class="nav-link scrollto" href="#pricing">{{ __('home_page.menu_pricing') }}</a></li>
-                <li><a class="nav-link scrollto" href="#contact">{{ __('home_page.menu_contact') }}</a></li>
-                <li><a class="nav-link scrollto" href="/app">{{ __('home_page.menu_login') }}</a></li>
-            </ul>
+            @if (request()->is('gdpr') || request()->is('cookies'))
+                <ul>
+                    <li><a class="nav-link scrollto" href="/">{{ __('home_page.menu_home') }}</a></li>
+                    <li><a class="nav-link scrollto" href="/#services">{{ __('home_page.menu_services') }}</a></li>
+                    <li><a class="nav-link scrollto" href="/#pricing">{{ __('home_page.menu_pricing') }}</a></li>
+                    <li><a class="nav-link scrollto" href="/#contact">{{ __('home_page.menu_contact') }}</a></li>
+                    <li><a class="nav-link scrollto" href="/app">{{ __('home_page.menu_login') }}</a></li>
+                </ul>
+            @else
+                <ul>
+                    <li><a class="nav-link scrollto" href="#hero">{{ __('home_page.menu_home') }}</a></li>
+                    <li><a class="nav-link scrollto" href="#services">{{ __('home_page.menu_services') }}</a></li>
+                    <li><a class="nav-link scrollto" href="#pricing">{{ __('home_page.menu_pricing') }}</a></li>
+                    <li><a class="nav-link scrollto" href="#contact">{{ __('home_page.menu_contact') }}</a></li>
+                    <li><a class="nav-link scrollto" href="/app">{{ __('home_page.menu_login') }}</a></li>
+                </ul>
+            @endif
+
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
 
