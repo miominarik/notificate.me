e1856e03dfbd7c5d0d7a3077c94dae8e79a17058
Miroslav Minárik <miominarik2@gmail.com>
2022-07-03 15:26:27 +0200

    Update superadminu

--- resources/views/superadmin/pages/users_modules.blade.php
+++ resources/views/superadmin/pages/users_modules.blade.php
@@ -8,6 +8,7 @@
                 <th scope="col">E-mail</th>
                 <th scope="col">Zľava</th>
                 <th scope="col">Modul SMS</th>
+                <th scope="col">Modul Kalendár</th>
             </tr>
             </thead>
             <tbody>
@@ -16,10 +17,12 @@
                     <th scope="row">{{$one_user_module->email}}</th>
                     <th scope="row">{{$one_user_module->discount_percent}} %</th>
                     <td class="ps-4 {{$one_user_module->module_sms == 1 ? 'text-success' : 'text-danger'}}">{{$one_user_module->module_sms == 1 ? html_entity_decode('&#10003;') : html_entity_decode('&#10005;')}}</td>
+                    <td class="ps-4 {{$one_user_module->module_calendar == 1 ? 'text-success' : 'text-danger'}}">{{$one_user_module->module_calendar == 1 ? html_entity_decode('&#10003;') : html_entity_decode('&#10005;')}}</td>
                 </tr>
             @empty
                 <tr>
                     <th scope="row">Žiadne dáta</th>
+                    <td></td>
                     <td></td>
                     <td></td>
                 </tr>
