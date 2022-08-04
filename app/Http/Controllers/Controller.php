cebe3c9bc69f8a081b7f48174cf97c391598a770
Miroslav Minárik <miominarik2@gmail.com>
2022-08-04 11:01:49 +0200

    Nezobrazovanie kalendára

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,16 +14,22 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+
+    public function __construct()
+    {
+        $this->key = '1b70b24466f996ec7778537030abab146b577267833dca7095e6a3c833469e588604f21b75dd2e9e2578c4eb8e0ad3c78e4c2c76fe52b75ce5a228cf1f1119ec';
+    }
+
     public function JWT_encode($data)
     {
         $data = [$data];
 
-        return JWT::encode($data, env('JWT_SECRET'), 'HS512');
+        return JWT::encode($data, $this->key, 'HS512');
     }
 
     public function JWT_decode($data)
     {
-        return (array)JWT::decode($data, new Key(env('JWT_SECRET'), 'HS512'));
+        return (array)JWT::decode($data, new Key($this->key, 'HS512'));
     }
 
 }
